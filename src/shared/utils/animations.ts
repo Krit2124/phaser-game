29898a99cc04cc5531b04620ef497cacd2cf021a
Krit2124 <kritserg2124@gmail.com>
2024-12-12 17:44:26 +0300

    Убрано повторное объявление анимаций

--- src/shared/utils/animations.ts
+++ src/shared/utils/animations.ts
@@ -1,26 +1,30 @@
 export function createCharacterAnimations(scene: Phaser.Scene) {
-  const animations = [
-    { key: "walk-down", frames: { start: 0, end: 3 } },
-    { key: "walk-left", frames: { start: 4, end: 7 } },
-    { key: "walk-right", frames: { start: 8, end: 11 } },
-    { key: "walk-up", frames: { start: 12, end: 15 } },
-  ];
+  if (!scene.anims.exists("walk-down")) {
+    const animations = [
+      { key: "walk-down", frames: { start: 0, end: 3 } },
+      { key: "walk-left", frames: { start: 4, end: 7 } },
+      { key: "walk-right", frames: { start: 8, end: 11 } },
+      { key: "walk-up", frames: { start: 12, end: 15 } },
+    ];
 
-  animations.forEach(({ key, frames }) => {
-    scene.anims.create({
-      key,
-      frames: scene.anims.generateFrameNumbers("character", frames),
-      frameRate: 10,
-      repeat: -1,
+    animations.forEach(({ key, frames }) => {
+      scene.anims.create({
+        key,
+        frames: scene.anims.generateFrameNumbers("character", frames),
+        frameRate: 10,
+        repeat: -1,
+      });
     });
-  });
+  }
 }
 
 export function createRocksAnimations(scene: Phaser.Scene) {
-  scene.anims.create({
-    key: "fallingRock",
-    frames: scene.anims.generateFrameNumbers("rock", { start: 0, end: 11 }),
-    frameRate: 10,
-    repeat: -1,
-  });
+  if (!scene.anims.exists("fallingRock")) {
+    scene.anims.create({
+      key: "fallingRock",
+      frames: scene.anims.generateFrameNumbers("rock", { start: 0, end: 11 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+  }
 }
